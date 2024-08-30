302f16a6581c0233477f6b826187b6dab4232fc1
sccastelli <sccastelli@miuandes.cl>
2024-08-30 16:03:51 -0400

    avances que funcionan(pero no sale nada en bar, beer y evento)

--- www-frontend/src/App.jsx
+++ www-frontend/src/App.jsx
@@ -1,24 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import React from 'react';
 import Home from './components/Home';
-import BeerList from './components/BeerList';
 import BarList from './components/BarList';
+import BeerList from './components/BeerList';
 import BarEvents from './components/BarEvents';
-import UserSearch from './components/UserSearch';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <Router>
-      <header className="header">
-        <h1>Beer Explorer</h1>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/beers" element={<BeerList />} />
-        <Route path="/bars" element={<BarList />} />
-        <Route path="/bars/:id/events" element={<BarEvents />} />
-        <Route path="/search" element={<UserSearch />} />
-      </Routes>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bar" element={<BarList />} />
+          <Route path="/beer" element={<BeerList />} />
+          <Route path="/events" element={<BarEvents />} />
+        </Routes>
+        <Navbar />
+      </div>
     </Router>
   );
 }
