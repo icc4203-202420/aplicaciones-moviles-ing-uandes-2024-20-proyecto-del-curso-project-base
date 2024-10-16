1dc9550b695b6bf4cbfe0f02f7cb05f1d80b36bf
sccastelli <sccastelli@miuandes.cl>
2024-10-16 18:02:37 -0300

    avances

--- hybrid-frontend/app/index.js
+++ hybrid-frontend/app/index.js
@@ -1,53 +1,66 @@
-// LoginScreen.js
 import React, { useState } from 'react';
 import { View, Text } from 'react-native';
-import { Input, Button } from '@rneui/themed';  // Cambiamos la importación a @rneui/themed
-import { useRouter } from 'expo-router';  // Usamos useRouter de expo-router
+import { Input, Button } from '@rneui/themed';
+import { useRouter } from 'expo-router'; // Usamos useRouter para la navegación
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const router = useRouter(); // Usamos useRouter para navegación
+  const router = useRouter(); // Usamos useRouter para navegar
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://10.33.0.124:3001/login', {
+      const response = await fetch('http://192.168.4.176:3001/api/v1/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({
+          session: { 
+            email,
+            password 
+          }
+        }),
       });
+  
+      const data = await response.json();
+  
       if (response.ok) {
-        // Navegamos a la página de inicio después de un inicio de sesión exitoso
+        // Capturamos el token JWT de la respuesta
+        const token = data.status.data.token;
+        // Almacenamos el token en AsyncStorage
+        await AsyncStorage.setItem('token', token);
+        // Navegamos a la pantalla de inicio
         router.push('/home');
       } else {
-        setErrorMessage('Invalid credentials, please try again.');
+        setErrorMessage(data.message || 'Invalid credentials, please try again.');
       }
     } catch (error) {
       setErrorMessage('Something went wrong, please try again later.');
     }
   };
-
+  
   return (
     <View>
       <Input
         placeholder="Email"
         value={email}
-        onChange={(event) => setEmail(event.nativeEvent.text)}
+        onChangeText={setEmail}
         keyboardType="email-address"
         autoCapitalize="none"
       />
       <Input
         placeholder="Password"
         value={password}
-        onChange={(event) => setPassword(event.nativeEvent.text)}
+        onChangeText={setPassword}
         secureTextEntry
       />
-
       {errorMessage ? <Text style={{ color: 'red' }}>{errorMessage}</Text> : null}
       <Button title="Login" onPress={handleLogin} />
+      
+      {/* Botón para redirigir a la pantalla de registro */}
+      <Button title="Register" onPress={() => router.push('/register')} />
     </View>
   );
 };
