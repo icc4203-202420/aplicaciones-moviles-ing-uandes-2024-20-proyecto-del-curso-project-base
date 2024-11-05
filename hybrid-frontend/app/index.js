643c2d6b83d2df17fc011b79cb2a5c0b311f6c57
Carolina <cbhong@miuandes.cl>
2024-11-04 21:57:26 -0300

    fix: login

--- hybrid-frontend/app/index.js
+++ hybrid-frontend/app/index.js
@@ -5,7 +5,7 @@ import { useRouter } from "expo-router";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NGROK_URL } from '@env';
-import { registerForPushNotificationsAsync } from "../util/Notifications";
+// import { registerForPushNotificationsAsync } from "../util/Notifications";
 const Login = () => {
   const router = useRouter();
   const [email, setEmail] = useState("");
@@ -15,7 +15,7 @@ const Login = () => {
   const handleLogin = async () => {
     setErrorMessage(""); // Limpiar el mensaje de error al intentar iniciar sesión
     try {
-      const pushToken = await registerForPushNotificationsAsync();
+      // const pushToken = await registerForPushNotificationsAsync();
       // console.log("PUSH TOKEN:", pushToken);
       // Realizar la solicitud de inicio de sesión
       const response = await axios.post(`${NGROK_URL}/api/v1/login`, {
